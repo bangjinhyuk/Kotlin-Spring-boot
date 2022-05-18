e2c92815063e2cf47611670784f7cf9fb90dbeff
bangjinhyuk <jinhyuk0103@gmail.com>
2022-05-18 22:22:58 +0900

    service 생성

---
+++ kotlin-crud/src/main/kotlin/com/example/kotlincrud/service/UserService.kt
@@ -0,0 +1,14 @@
+package com.example.kotlincrud.service
+
+import com.example.kotlincrud.model.dto.RequestUser
+import com.example.kotlincrud.model.dto.SimpleUser
+import com.example.kotlincrud.model.entity.User
+
+interface UserService {
+
+    fun findAllUsers(): List<User>
+    fun createUser(requestUser: RequestUser): SimpleUser
+    fun login(name: String, password: String): Any?
+    fun update(requestUser: RequestUser): SimpleUser
+
+}
---
+++ kotlin-crud/src/main/kotlin/com/example/kotlincrud/service/UserServiceImpl.kt
@@ -0,0 +1,55 @@
+package com.example.kotlincrud.service
+
+import com.example.kotlincrud.exception.BaseException
+import com.example.kotlincrud.exception.BaseResponseCode
+import com.example.kotlincrud.model.dto.RequestUser
+import com.example.kotlincrud.model.dto.SimpleUser
+import com.example.kotlincrud.model.entity.User
+import com.example.kotlincrud.model.repository.UserRepository
+import org.springframework.boot.context.properties.bind.Bindable.listOf
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import java.sql.DriverManager.println
+
+@Service
+class UserServiceImpl(
+    private val userRepository: UserRepository
+) : UserService {
+
+    @Transactional(readOnly = true)
+    override fun findAllUsers(): List<User> {
+        return userRepository.findAllBy()
+    }
+
+    @Transactional
+    override fun createUser(requestUser: RequestUser): SimpleUser {
+
+        var user = User(
+            name = requestUser.name,
+            password = requestUser.password,
+            gender = requestUser.gender
+        )
+
+        return userRepository.save(user).createSimpleUser()
+    }
+
+    @Transactional(readOnly = true)
+    override fun login(name: String, password: String): Any? {
+        var user = userRepository.findByNameAndPassword(name,password)
+        if(user.isPresent)
+            return "Token"
+        else
+            throw BaseException(BaseResponseCode.USER_NOT_FOUND)
+    }
+
+    @Transactional
+    override fun update(requestUser: RequestUser): SimpleUser {
+        var user = userRepository.findByName(requestUser.name)
+
+        if(user.isPresent)
+            return user.get().updateUser(requestUser)
+        else
+            throw BaseException(BaseResponseCode.USER_NOT_FOUND)
+    }
+
+}
