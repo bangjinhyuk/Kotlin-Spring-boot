d5b6dd36b5512698c7f12d4aeae202ae8c68099a
bangjinhyuk <jinhyuk0103@gmail.com>
2022-05-26 00:01:31 +0900

    Valid 예외 핸들러 생성

--- kotlin-crud/src/main/kotlin/com/example/kotlincrud/exception/ExceptionHandler.kt
+++ kotlin-crud/src/main/kotlin/com/example/kotlincrud/exception/ExceptionHandler.kt
@@ -2,6 +2,7 @@ package com.example.kotlincrud.exception
 
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import java.sql.DriverManager.println
@@ -11,10 +12,21 @@ class ExceptionHandler {
 
     @ExceptionHandler(BaseException::class)
     protected fun handleBaseException(e:BaseException): ResponseEntity<Any> {
-        System.out.println("${e.baseResponseCode.message} - ${e.baseResponseCode.status}")
         return ResponseEntity.status(e.baseResponseCode.status)
             .body(BaseRes(e.baseResponseCode.status, e.baseResponseCode.message))
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    protected fun handleConstraintViolationException(e: MethodArgumentNotValidException): ResponseEntity<Any> {
+        val ee = mutableMapOf<String, String>()
+        for (error in e.fieldErrors) {
+            ee.put(error.field, error.defaultMessage.toString())
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            .body(BaseRes(HttpStatus.BAD_REQUEST, ee.toString()))
+    }
+
+
     data class BaseRes (val status: HttpStatus, val message: String)
 }
 
