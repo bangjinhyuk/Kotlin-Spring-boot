be64683b65792e8490f5294ecbcfe19c5a55699c
bangjinhyuk <jinhyuk0103@gmail.com>
2022-05-27 23:23:23 +0900

    JpaAuditing config 파일 생성

---
+++ kotlin-crud/src/main/kotlin/com/example/kotlincrud/config/JpaAuditingConfig.kt
@@ -0,0 +1,12 @@
+package com.example.kotlincrud.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+/**
+ * Created by bangjinhyuk on 2022/05/27.
+ */
+@Configuration
+@EnableJpaAuditing
+class JpaAuditingConfig {
+}
